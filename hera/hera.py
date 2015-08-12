6237e299ffac8196d8fc42158d6e10bfe86d42d0
lofyer <lofyer@gmail.com>
2015-08-13 00:54:28 +0800

    update

--- hera/hera.py
+++ hera/hera.py
@@ -1,5 +1,23 @@
+#!/usr/bin/env python
 import logging
 import os
 import yahoo_finance
+import ConfigParser
 
-logging.basicConfig(filename = os.path.join(os.getcwd(), 'hera.log'), level = logging.WARN, filemode = 'w', format = '%(asctime)s - %(levelname)s: %(message)s')
+# Read configuration
+config = ConfigParser.RawConfigParser()
+config.read("hera.conf")
+CSV_FILES = config.get("Stock Lists","csv_files")
+
+# DEBUG INFO WARN ERROR
+logging.basicConfig(filename = os.path.join(os.getcwd(), 'hera.log'), level = logging.INFO, filemode = 'aw', format = '%(asctime)s - %(levelname)s: %(message)s')
+
+logging.info("Opening csv files...")
+lists = []
+for i in CSV_FILES.split(","):
+    f = open(i,"r")
+    lists = lists + f.readlines()
+    f.close()
+
+for i in lists:
+    print i
