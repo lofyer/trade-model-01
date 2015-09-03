4d4903efc77a20bb035e35d3f1c0a601a8c6405c
lofyer <lofyer@gmail.com>
2015-09-04 01:50:20 +0800

    log

--- hera/hera.py
+++ hera/hera.py
@@ -12,11 +12,12 @@ CSV_FILES = config.get("Stock Lists","csv_files")
 # DEBUG INFO WARN ERROR
 logging.basicConfig(filename = os.path.join(os.getcwd(), 'hera.log'), level = logging.INFO, filemode = 'aw', format = '%(asctime)s - %(levelname)s: %(message)s')
 
-logging.info("Opening csv files...")
 lists = []
 for i in CSV_FILES.split(","):
+    logging.info("Opening csv files... %s" % i )
     f = open(i,"r")
     lists = lists + f.readlines()
+    logging.info("Closing csv files... %s" % i)
     f.close()
 
 for i in lists:
