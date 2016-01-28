2e396fa7b3efcbfaa8ab174913baa69c0f7006e0
lofyer <lofyer@gmail.com>
2016-01-28 12:19:38 +0800

    update

--- athena/www/rss/rss/index.php
+++ athena/www/rss/rss/index.php
@@ -54,8 +54,11 @@ if (!empty($_GET['force']) && $_GET['force'] == 'true')
 // all that other good stuff.  The feed's information will not be available to SimplePie before
 // this is called.
 $feed->set_cache_location('/var/www/forex/rss/rss/cache');
+#$feed->set_cache_location('mysql://root:pswd4mysql@localhost:3306/simplepie_rss');
 $feed->set_cache_duration(1800);
+$feed->set_timeout(30);
 $feed->enable_cache(True);
+//$feed->force_feed(true);
 $success = $feed->init();
 
 // We'll make sure that the right content type and character encoding gets set automatically.
@@ -139,38 +142,61 @@ $feed->handle_content_type();
 			?>
 
 			<!-- Here are some sample feeds. -->
-                        <p>News</p>
-			<p class="sample_feeds"><strong>Tech:</strong>
+
+		        <p class="sample_feeds">
+			<p align="center">News 新闻</p>
+			<strong>Tech:</strong>
 			<a href="?feed=https://lwn.net/headlines/rss">LWN News</a>,
+			<a href="?feed=http://feeds.feedburner.com/TechCrunch/">TechCrunch</a>,
 			<a href="?feed=http://rss.cnbeta.com/rss">cnBeta</a>
-			<p class="sample_feeds"><strong>World:</strong>
-			<a href="?feed=http://www.people.com.cn/rss/politics.xml">人民日报－国内政治</a>,
-			<a href="?feed=http://www.people.com.cn/rss/world.xml">人民日报－国际新闻</a>,
-			<a href="?feed=http://www.people.com.cn/rss/finance.xml">人民日报－经济新闻</a>,
-			<a href="?feed=http://www.people.com.cn/rss/haixia.xml">人民日报－海峡两岸</a>,
-			<a href="?feed=http://www.people.com.cn/rss/haixia.xml">人民日报－教育新闻</a>,
+			</br><strong>World:</strong>
+			<a href="?feed=http://www.people.com.cn/rss/politics.xml">人民日报-国内政治</a>,
+			<a href="?feed=http://www.people.com.cn/rss/world.xml">人民日报-国际新闻</a>,
+			<a href="?feed=http://www.people.com.cn/rss/finance.xml">人民日报-经济新闻</a>,
+			<a href="?feed=http://www.people.com.cn/rss/haixia.xml">人民日报-海峡两岸</a>,
+			<a href="?feed=http://www.people.com.cn/rss/edu.xml">人民日报-教育新闻</a>,
+			<a href="?feed=http://money.163.com/special/00252EQ2/yaowenrss.xml">网易要闻</a>,
+			<a href="?feed=http://money.163.com/special/00252EQ2/toutiaorss.xml">网易财经</a>,
+			<a href="?feed=http://money.163.com/special/00252EQ2/macrorss.xml">网易宏观</a>,
 			<a href="?feed=http://newsrss.bbc.co.uk/rss/newsonline_world_edition/front_page/rss.xml" title="World News">BBC News</a>,
 			<a href="?feed=http://newsrss.bbc.co.uk/rss/chinese/simp/news/rss.xml" title="Test: GB2312 Encoding">BBC China</a>,
 			<a href="?feed=http://rss.news.yahoo.com/rss/topstories" title="World News">Yahoo! News</a>,
-			<a href="?feed=http://rss.cnn.com/rss/cnn_topstories.rss" title="World News">CNN</a>,
+			<a href="?feed=http://rss.cnn.com/rss/cnn_topstories.rss" title="World News">CNN News</a>,
 			<a href="?feed=http://news.google.com/?output=rss" title="World News">Google News</a>
-			<p class="sample_feeds"><strong>Forex:</strong>
+			</br><strong>Forex:</strong>
 			<a href="?feed=http://www.fxtimes.com/feed" title="World News">Forex Times</a>,
 			<a href="?feed=http://rss.forexfactory.net/news/all.xml">Forex Factory</a>,
 			<a href="?feed=http://rss.forexfactory.net/forum/all.xml">Forex Factory Forum</a>,
-			<a href="?feed=http://xml.fxstreet.com/news/forex-news/index.xml">Forex Street</a>,
-                        <p>Banks</p>
-			<p class="sample_feeds"><strong>BIS国际结算银行:</strong>
-			<a href="?feed=http://www.bis.org/list/bis/index.rss">BIS</a>,
+			<a href="?feed=http://xml.fxstreet.com/news/forex-news/index.xml">Forex Street</a>
+			</br><strong>Economist:</strong>
+			<a href="?feed=http://www.economist.com/sections/business-finance/rss.xml">Business and finance</a>,
+			<a href="?feed=http://www.economist.com/sections/economics/rss.xml">Economics</a>,
+			<a href="?feed=http://www.economist.com/sections/science-technology/rss.xml">Science and technology</a>,
+			<a href="?feed=http://www.economist.com/sections/culture/rss.xml">Culture</a>,
+			<a href="?feed=http://www.economist.com/sections/leaders/rss.xml">Leaders</a>,
+			<a href="?feed=http://www.economist.com/blogs/gametheory/index.xml">Game theory</a>,
+			<a href="?feed=http://www.economist.com/sections/markets-data/rss.xml">Markets and data</a>,
+			<a href="?feed=http://www.economist.com/sections/international/rss.xml">Internaltional</a>,
+			<a href="?feed=http://www.economist.com/sections/obituary/rss.xml">Obituary</a>,
+			<a href="?feed=http://www.economist.com/sections/united-states/rss.xml">United States</a>,
+			<a href="?feed=http://www.economist.com/sections/china/rss.xml">China</a>,
+			<a href="?feed=http://www.economist.com/sections/americas/rss.xml">Americas</a>,
+			<a href="?feed=http://www.economist.com/sections/middle-east-africa/rss.xml">Middle east and Africa</a>,
+			<a href="?feed=http://www.economist.com/sections/britain/rss.xml">Britain</a>,
+			<a href="?feed=http://www.economist.com/sections/europe/rss.xml">Europe</a>,
+			<a href="?feed=http://www.economist.com/sections/Asia/rss.xml">Asia</a>
+            <p align="center">Banks 银行组织</p>
+			</br><strong>BIS国际结算银行:</strong>
+			<a href="?feed=http://www.bis.org/list/bis/index.rss">BIS All</a>,
 			<a href="?feed=http://www.bis.org/list/statistics/index.rss">BIS Statistic</a>,
-			<a href="?feed=http://www.bis.org/list/press_releases/index.rss">BIS Press</a>
+			<a href="?feed=http://www.bis.org/list/press_releases/index.rss">BIS Press</a>,
 			<a href="?feed=http://www.bis.org/list/papers/index.rss">BIS Papers</a>
-			<p class="sample_feeds"><strong>FRS美联储:</strong>
+			</br><strong>FRB美联储:</strong>
 			<a href="?feed=http://www.federalreserve.gov/feeds/press_all.xml">Press Releases</a>,
 			<a href="?feed=http://www.federalreserve.gov/feeds/data/H10_H10.XML">Forex Exchange Rate</a>,
 			<a href="?feed=http://www.federalreserve.gov/feeds/datadownload.xml">Data Download</a>,
 			<a href="?feed=http://www.federalreserve.gov/feeds/speeches.xml">Speeches</a>
-			<p class="sample_feeds"><strong>ECB欧洲中央银行:</strong>
+			</br><strong>ECB欧洲中央银行:</strong>
 			<a href="?feed=https://www.ecb.europa.eu/rss/press.html">Press Releases</a>,
 			<a href="?feed=https://www.ecb.europa.eu/rss/statpress.html">Statistical Press Releases</a>,
 			<a href="?feed=https://www.ecb.europa.eu/rss/pub.html">Publications</a>,
@@ -179,59 +205,60 @@ $feed->handle_content_type();
 			<a href="?feed=https://www.ecb.europa.eu/rss/operations.html">Recent open market</a>,
 			<a href="?feed=https://www.ecb.europa.eu/rss/procurements.html">Open procurements</a>,
 			<a href="?feed=https://www.ecb.europa.eu/rss/yc.html">Yield curve</a>
-			<p class="sample_feeds"><strong>BOJ日本银行:</strong>
+			</br><strong>BOJ日本银行:</strong>
 			<a href="?feed=http://www.boj.or.jp/en/rss/whatsnew.xml">Recent News</a>
-			<p class="sample_feeds"><strong>SNB瑞士国民银行:</strong>
+			</br><strong>SNB瑞士国民银行:</strong>
 			<a href="?feed=http://www.snb.ch/selector/en/mmr/news/rss">Recent News</a>,
 			<a href="?feed=http://www.snb.ch/selector/en/mmr/pressrel/rss">Press</a>,
 			<a href="?feed=http://www.snb.ch/selector/en/mmr/mopo/rss">Monetary policy</a>,
 			<a href="?feed=http://www.snb.ch/selector/en/mmr/statistics/rss">Statistical publications</a>,
 			<a href="?feed=http://www.snb.ch/selector/en/mmr/papers/rss">Working Papers</a>
-			<p class="sample_feeds"><strong>BOE英格兰银行:</strong>
+			</br><strong>BOE英格兰银行:</strong>
 			<a href="?feed=http%3A%2F%2Fwww.bankofengland.co.uk%2F_layouts%2FBOE.internetCMS%2Ffeeddirector.aspx%3FSourcePage%3D%252FPages%252FSiteFeed%252Easpx%26RssSource%3DNews%2520Release%2520RSS">Recent News</a>,
 			<a href="?feed=http%3A%2F%2Fwww.bankofengland.co.uk%2F_layouts%2FBOE.internetCMS%2Ffeeddirector.aspx%3FSourcePage%3D%252FPages%252FSiteFeed%252Easpx%26RssSource%3DPublications%2520RSS">Publications</a>,
 			<a href="?feed=http%3A%2F%2Fwww.bankofengland.co.uk%2F_layouts%2FBOE.internetCMS%2Ffeeddirector.aspx%3FSourcePage%3D%252FPages%252FSiteFeed%252Easpx%26RssSource%3DSpeeches%2520RSS">Speeches</a>,
 			<a href="?feed=http%3A%2F%2Fwww.bankofengland.co.uk%2F_layouts%2FBOE.internetCMS%2Ffeeddirector.aspx%3FSourcePage%3D%252FPages%252FSiteFeed%252Easpx%26RssSource%3DPRA%2520RSS">Prudential Regulation Authority</a>,
 			<a href="?feed=http%3A%2F%2Fwww.bankofengland.co.uk%2F_layouts%2FBOE.internetCMS%2Ffeeddirector.aspx%3FSourcePage%3D%252FPages%252FSiteFeed%252Easpx%26RssSource%3DStatistical%2520Releases%2520RSS">Statistical Releases</a>
-			<p class="sample_feeds"><strong>RBA澳大利亚储备银行:</strong>
+			</br><strong>RBA澳大利亚储备银行:</strong>
 			<a href="?feed=http://www.rba.gov.au/rss/rss-cb-media-releases.xml">Media Releases</a>,
 			<a href="?feed=http://www.rba.gov.au/rss/rss-cb-speeches.xml">Speeches</a>,
 			<a href="?feed=http://www.rba.gov.au/rss/rss-cb-exchange-rates.xml">Exchange Rates</a>,
 			<a href="?feed=http://www.rba.gov.au/rss/rss-cb-rdp.xml">Research Papers</a>,
 			<a href="?feed=http://www.rba.gov.au/rss/rss-cb-foi.xml">Freedom of Information</a>,
-			<a href="?http://www.rba.gov.au/rss/rss-cb-changes-to-tables.xml">Changes to Statistical Tables</a>
-			<p class="sample_feeds"><strong>RBNZ新西兰储备银行:</strong>
+			<a href="?feed=http%3A%2F%2Fwww.rba.gov.au%2Frss%2Frss-cb-changes-to-tables.xml">Changes to Statistical Tables</a>
+			</br><strong>RBNZ新西兰储备银行:</strong>
 			<a href="?feed=http://www.rbnz.govt.nz/feeds/news.xml">News Releases</a>
-			<p class="sample_feeds"><strong>OECD经济合作与发展组织:</strong>
+			</br><strong>OECD经济合作与发展组织:</strong>
 			<a href="?feed=http://www.oecd.org/index.xml">Front Page</a>,
 			<a href="?feed=http://www.oecd.org/newsroom/index.xml">News Releases</a>,
 			<a href="?feed=http://feeds.feedburner.com/OecdObserver">OECD Observer</a>,
 			<a href="?feed=http://oecdinsights.org/feed/">Insights Blog</a>
-                        <p>Statistics</p>
-			<p class="sample_feeds"><strong>STATS.CN中国统计局:</strong>
+            <p align="center">Statistics 统计局</p>
+			</br><strong>STATS.CN中国统计局:</strong>
 			<a href="?feed=http://www.stats.gov.cn/tjsj/zxfb/rss.xml">最新发布</a>,
 			<a href="?feed=http://www.stats.gov.cn/tjsj/sjjd/rss.xml">数据解读</a>
-			<p class="sample_feeds"><strong>ESA美国经济统计局:</strong>
+			</br><strong>ESA美国经济统计局:</strong>
 			<a href="?feed=http://www.esa.doc.gov/rss.xml">News</a>
-			<p class="sample_feeds"><strong>BLS美国劳工统计局:</strong>
+			</br><strong>BLS美国劳工统计局:</strong>
 			<a href="?feed=http://www.bls.gov/feed/cpi.rss">CPI</a>
-			<p class="sample_feeds"><strong>CENSUS美国人口普查局:</strong>
+			</br><strong>CENSUS美国人口普查局:</strong>
 			<a href="?feed=http://www.census.gov/economic-indicators/indicator.xml">Economic Indicators</a>,
 			<a href="?feed=http://www.census.gov/newsroom/press-releases/by-year.xml">Press Releases</a>,
 			<a href="?feed=http://globalreach.blogs.census.gov/feed/">Global Reach</a>,
 			<a href="?feed=http://blogs.census.gov/feed/">Random Samplings</a>,
 			<a href="?feed=http://researchmatters.blogs.census.gov/feed/">Research Matters</a>
-			<p class="sample_feeds"><strong>EC.EUROPA欧盟委员会欧洲统计局:</strong>
+			</br><strong>EC.EUROPA欧盟委员会欧洲统计局:</strong>
 			<a href="?feed=http://ec.europa.eu/eurostat/cache/RSS/rss_estat_news.xml">News Releases</a>,
 			<a href="?feed=http://ec.europa.eu/eurostat/cache/RSS/rss_estat_sif.xml">Statistic in focus</a>
-			<p class="sample_feeds"><strong>STAT.JP日本统计局和统计中心:</strong>
+			</br><strong>STAT.JP日本统计局和统计中心:</strong>
 			<a href="?feed=http://www.stat.go.jp/whatsnew/news.rdf">News Releases(Japanese)</a>
-			<p class="sample_feeds"><strong>BFS瑞士联邦统计办公室:</strong>
+			</br><strong>BFS瑞士联邦统计办公室:</strong>
 			<a href="?feed=http://www.bfs.admin.ch/bfs/portal/en/index/news/publikationen.rss2.html">Publications</a>
-			<p class="sample_feeds"><strong>STA.UK英国国家统计局:</strong>
+			</br><strong>STA.UK英国国家统计局:</strong>
 			<a href="?feed=https://www.gov.uk/government/statistics.atom?publication_filter_option=statistics">Statistics</a>
-			<p class="sample_feeds"><strong>ABS.AU澳大利亚统计局:</strong>
+			</br><strong>ABS.AU澳大利亚统计局:</strong>
 			<a href="?feed=http://www.abs.gov.au/AUSSTATS/wmdata.nsf/activerss/headline_rss/$File/headline_rss.xml">Statistics Headlines</a>
+		</p>
 
 		</div>
 
@@ -243,12 +270,12 @@ $feed->handle_content_type();
 
 					<!-- If the feed has a link back to the site that publishes it (which 99% of them do), link the feed's title to it. -->
 					<h3 class="header">
-					<?php 
+					<?php
 						$link = $feed->get_link();
 						$title = $feed->get_title();
-						if ($link) 
-						{ 
-							$title = "<a href='$link' title='$title'>$title</a>"; 
+						if ($link)
+						{
+							$title = "<a href='$link' title='$title'>$title</a>";
 						}
 						echo $title;
 					?>
